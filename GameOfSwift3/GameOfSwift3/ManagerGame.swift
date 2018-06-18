6b81bb9822d875fb42ad27ed175bc7f5b196e320
ElsaJ <elsa.jeannin@gmail.com>
2018-06-18 10:27:11 +0200

    Created project and add manager game file

---
+++ GameOfSwift3/GameOfSwift3/ManagerGame.swift
@@ -0,0 +1,29 @@
+//
+//  ManagerGame.swift
+//  GameOfSwift3
+//
+//  Created by Elsa Jeannin on 18/06/2018.
+//  Copyright © 2018 Elsa Jeannin. All rights reserved.
+//
+
+import Foundation
+
+// Create Game Manager to control the Game
+
+
+class GameManager {
+    
+    let numberofplayer = 2
+    
+// function Start the game
+    
+    func startGame() {
+        print("Welcome to the Game")
+        print("Please enter your name.")
+        var playerOne = readLine()
+        print("Please enter your name.")
+        var playerTwo = readLine()
+    }
+}
+    
+
---
+++ GameOfSwift3/GameOfSwift3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GameOfSwift3
+//
+//  Created by Elsa Jeannin on 18/06/2018.
+//  Copyright © 2018 Elsa Jeannin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
