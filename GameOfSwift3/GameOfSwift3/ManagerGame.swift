0560f849ac06a408bcb145c52ee507072fe04ba3
ElsaJ <elsa.jeannin@gmail.com>
2018-07-29 17:47:40 +0200

    change ManagerGame and simplified Player

--- GameOfSwift3/GameOfSwift3/ManagerGame.swift
+++ GameOfSwift3/GameOfSwift3/ManagerGame.swift
@@ -11,21 +11,118 @@ import Foundation
 // Create Game Manager to control the Game
 
 class GameManager {
-    let numberOfPlayers = 2
-    var numberOfTeam = 2
-    var playerOne = Player()
-    var playerTwo = Player()
+    private var numberOfCharacter = 3
+    private var playerOne = Player()
+    private var playerTwo = Player()
+    private var chosenNames = [String] ()
     
     // function Start the game
     
     func initGame() {
         print("Welcome to Game of Swift 3")
         playerOne.nameYourself()
-        playerOne.makeYourTeam()
-        playerTwo.nameYourself()
-        playerTwo.makeYourTeam()
-       
+        let team1 = createTeam()
+        playerOne.team = team1
         playerOne.resume()
+        playerTwo.nameYourself()
+        let team2 = createTeam()
+        playerTwo.team = team2
         playerTwo.resume()
+    }
+    
+    func createTeam() -> [Character] {
+        var team = [Character]()
+        print("Now make your team !")
+        showList()
+        while team.count < numberOfCharacter {
+            let character = characterChoice()
+            team.append(character)
+        }
+        
+        return team
+    }
+    
+    private func showList() {
+        
+        print("This is the list of characters available :"
+            + "\n \(CharacterType.Fighter.rawValue)/ Fighter"
+            + "\n \(CharacterType.Colossus.rawValue)/ Colossus"
+            + "\n \(CharacterType.Dwarf.rawValue)/ Dwarf"
+            + "\n \(CharacterType.Fairy.rawValue)/ Fairy"
+            + "\n \(CharacterType.Magus.rawValue)/ Magus")
+    }
+    
+    private func characterChoice() -> Character {
+        let characterType = chooseCharacterType()
+        let characterName = chooseCharacterName()
+        let characterLifePoint = getLifePoint(type: characterType)
+        let characterTools = getTools(type: characterType)
+        
+        return Character(name: characterName, type: characterType, maxLifePoint: characterLifePoint, tools: characterTools)
+    }
+    
+    private func chooseCharacterType() -> CharacterType {
+        print("Use number to choose character for your team:")
+        if let characterTypeString = readLine(),
+            let typeRawValue = Int(characterTypeString),
+            let characterType = CharacterType(rawValue: typeRawValue) {
+            return characterType
+        } else {
+            print("You have to choose number beetween one to five, try again:")
+            return chooseCharacterType()
+        }
+    }
+    
+    private func chooseCharacterName() -> String {
+        print("Now give him his own name:")
+        if let characterName = readLine(),
+            nameIsUnique(chosenCharacterName: characterName) == false {
+            chosenNames.append(characterName)
+            return characterName
+        } else {
+            return chooseCharacterName()
+        }
+    }
+    
+    private func nameIsUnique(chosenCharacterName: String) -> Bool {
+        var choice = true
+        if  !chosenNames.contains(chosenCharacterName) {
+            choice = false
+        } else {
+            print("\(chosenCharacterName) already exists")
+            choice = true
+        }
+        return choice
+        
+    }
+    
+    private func getLifePoint(type: CharacterType) -> Int {
+        switch type {
+        case .Fighter:
+            return 100
+        case .Colossus:
+            return 150
+        case .Dwarf:
+            return 120
+        case .Fairy:
+            return 180
+        case .Magus:
+            return 80
+        }
+    }
+    
+    private func getTools(type: CharacterType) -> String {
+        switch type {
+        case .Fighter:
+            return ("Sword")
+        case .Colossus:
+            return ("Mace")
+        case .Dwarf:
+            return ("Axe")
+        case .Fairy:
+            return ("Saber")
+        case .Magus:
+            return ("Plant")
+        }
     }
 }
--- GameOfSwift3/GameOfSwift3/Player.swift
+++ GameOfSwift3/GameOfSwift3/Player.swift
@@ -11,28 +11,18 @@ import Foundation
 // Creation class Player
 
 class Player {
-    private var team: [Character] = []
-    private var numberOfCharacter = 3
+    var team: [Character] = []
     private var name = ""
-    private var chosenNames = [String] ()
     
     func nameYourself() {
         print("Hello player, please enter your name: ")
-        if let name = readLine() {
+        if let name = readLine(),
+            name != "" {
             print("Welcome \(name) !")
             self.name = name
         } else  {
             print("error in naming, try again:")
             nameYourself()
-        }
-    }
-    
-    func makeYourTeam(){
-        print("Now make your team !")
-        showList()
-        while team.count < numberOfCharacter {
-            let character = characterChoice()
-            team.append(character)
         }
     }
     
@@ -42,90 +32,4 @@ class Player {
             print("\(character.type) \(character.name) who start with \(character.maxLifePoint) lifepoints and \(character.tools) in his hand")
         }
     }
-    
-    private func showList() {
-        
-        print("This is the list of characters available :"
-            + "\n \(CharacterType.Fighter.rawValue)/ Fighter"
-            + "\n \(CharacterType.Colossus.rawValue)/ Colossus"
-            + "\n \(CharacterType.Dwarf.rawValue)/ Dwarf"
-            + "\n \(CharacterType.Fairy.rawValue)/ Fairy"
-            + "\n \(CharacterType.Magus.rawValue)/ Magus")
-    }
-    
-    private func characterChoice() -> Character {
-        let characterType = chooseCharacterType()
-        let characterName = chooseCharacterName()
-        let characterLifePoint = getLifePoint(type: characterType)
-        let characterTools = getTools(type: characterType)
-        
-        return Character(name: characterName, type: characterType, maxLifePoint: characterLifePoint, tools: characterTools)
-    }
-    
-    private func chooseCharacterType() -> CharacterType {
-        print("Use number to choose character for your team:")
-        if let characterTypeString = readLine(),
-            let typeRawValue = Int(characterTypeString),
-            let characterType = CharacterType(rawValue: typeRawValue) {
-            return characterType
-        } else {
-            print("You have to choose number beetween one to five, try again:")
-            return chooseCharacterType()
-        }
-    }
-    
-    private func chooseCharacterName() -> String {
-        print("Now give him his own name:")
-        if let characterName = readLine(),
-            nameIsUnique(chosenCharacterName: characterName) == false {
-            chosenNames.append(characterName)
-            print(chosenNames)
-        return characterName
-        } else {
-            return chooseCharacterName()
-        }
-    }
-    
-    private func nameIsUnique(chosenCharacterName: String) -> Bool {
-        var choice = true
-            if  !chosenNames.contains(chosenCharacterName) {
-                choice = false
-            } else {
-                print("\(chosenCharacterName) already exists")
-                choice = true
-            }
-        return choice
-        
-        }
-    
-    private func getLifePoint(type: CharacterType) -> Int {
-        switch type {
-        case .Fighter:
-            return 100
-        case .Colossus:
-            return 150
-        case .Dwarf:
-            return 120
-        case .Fairy:
-            return 180
-        case .Magus:
-            return 80
-        }
-    }
-    
-    private func getTools(type: CharacterType) -> String {
-        switch type {
-        case .Fighter:
-            return ("Sword")
-        case .Colossus:
-            return ("Mace")
-        case .Dwarf:
-            return ("Axe")
-        case .Fairy:
-            return ("Saber")
-        case .Magus:
-            return ("Plant")
-        }
-    }
-    
 }
